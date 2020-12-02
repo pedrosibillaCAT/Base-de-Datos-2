71615a0d9922acd5419ad1da6c776cbe5db9e437
pedrosibillaCAT <40602774+pedrosibillaCAT@users.noreply.github.com>
2020-12-02 10:25:42 -0300

    Add files via upload

---
+++ Redis.py
@@ -0,0 +1,28 @@
+import redis
+from datetime import date
+r = redis.Redis(
+host='localhost',
+port=6257)
+print("Bienvenido a su listado de tareas del dia")
+actividad = 'a'
+today = str(date.today())
+fecha = 'a'
+
+while comida!="":
+    comida = input("Inserte su activad mas reciente o presione enter para salir ")
+    if comida == "":
+        pass
+    else:
+        r.lpush(today, actividad)
+print('Las tareas del dia '+ today+' fueron:')
+for a in r.lrange(today, 0, -1):
+    print(str(a.decode('utf-8')))
+
+print('Estas son todas las fechas que en las que se guardaron las tareas realizadas: ')
+for key in r.scan_iter():
+    print(str(key.decode('utf-8')))
+
+while fecha!='':
+    fecha=input('Inserte la fecha de la que desea saber que actividad realizo o presione enter para salir ')
+    for x in r.lrange(fecha, 0, -1):
+        print(str(x.decode('utf-8')))
